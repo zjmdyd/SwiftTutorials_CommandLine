7cd1e745d9acf33823d1fdaad1f11d69399571bf
zjmdyd <zjmdyd@163.com>
2016-01-13 09:56:56 +0800

    Initial Commit

---
+++ SwiftTutorials_CommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTutorials_CommandLine
+//
+//  Created by ZJ on 1/13/16.
+//  Copyright © 2016 ZJ. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
